5b8b118fd4342333dc0cbbcd6f92fdfabf15ef44
sd19671 <scott.dugan@hotmail.com>
2021-08-14 17:21:24 -0700

    merge

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { Auth } from "aws-amplify";
 import { Authenticator } from "aws-amplify-react";
 import styled from "@emotion/styled";
-import '@fontsource/roboto';
 
 import awsExports from "./aws-exports";
 import Screens from "./components/Screens";
@@ -14,6 +13,43 @@ const Title = styled("h1")`
   margin-bottom: 8px;
 `;
 
+<<<<<<< HEAD
+=======
+const theme = {
+  formContainer: {
+    margin: 0,
+    padding: "8px 24px 24px"
+  },
+  formSection: {
+    backgroundColor: "#ffffff",
+    borderRadius: "4px"
+  },
+  sectionHeader: {
+    color: "#fff"
+  },
+  sectionFooterSecondaryContent: {
+    color: "#303952"
+  },
+  inputLabel: {
+    color: "#fff"
+  },
+  input: {
+    backgroundColor: "#f4f9f4",
+    color: "#fff"
+  },
+  hint: {
+    color: "#fff"
+  },
+  button: {
+    borderRadius: "3px",
+    backgroundColor: "#a7d7c5"
+  },
+  a: {
+    color: "#a7d7c5"
+  }
+};
+
+>>>>>>> parent of 8359140... theme update
 function App() {
   const [state, setState] = useState({ isLoggedIn: false, user: null });
 
@@ -42,7 +78,11 @@ function App() {
           }
         }}
         amplifyConfig={awsExports}
+<<<<<<< HEAD
        
+=======
+        //theme={theme}
+>>>>>>> parent of 8359140... theme update
       />
     </>
   );
